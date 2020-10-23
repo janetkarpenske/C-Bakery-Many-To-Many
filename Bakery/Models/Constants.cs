d6c699768c5b3533d5c2b76c45a3a08c12292e2a
Janet Karpenske <janetkarpenske@gmail.com>
2020-10-23 15:39:33 -0700

    Add functionality to create admin

---
+++ Bakery/Models/Constants.cs
@@ -0,0 +1,7 @@
+namespace Bakery
+{
+  public static class Constants
+  {
+    public const string AdministratorRole = "Administrator";
+  }
+}
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,5 +1,13 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Bakery
 {
@@ -13,8 +21,26 @@ namespace Bakery
         .UseIISIntegration()
         .UseStartup<Startup>()
         .Build();
+        InitializeDatabase(host);
+        host.Run();
+    }
+    private static void InitializeDatabase(IWebHost host)
+    {
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
 
-      host.Run();
+        try
+        {
+          SeedData.InitializeAsync(services).Wait();
+        }
+        catch (Exception ex)
+        {
+          var logger = services
+            .GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "Error occurred seeding the DB.");
+        }
+      }
     }
   }
 }
